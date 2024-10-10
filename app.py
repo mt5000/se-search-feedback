f0d5947623200a5d7b1b72ec8b076b10effe1ac3
mt5000 <matt@inclusively.com>
2024-10-10 14:50:22 -0400

    Add feedback form to Streamlit app
    Implemented a feedback system that presents users with search queries and gathers their ratings and comments. Also added a requirements file to ensure the necessary dependencies are installed.

--- app.py
+++ app.py
@@ -1,3 +1,63 @@
 import streamlit as st
 import pandas as pd
+import random
 
+@st.cache(allow_output_mutation=True)
+def import_dataframe(filepath: str = "./search_output_for_eval_preprocessed.csv"):
+    data = pd.read_csv(filepath)
+    return data
+
+def get_random_row(df: pd.DataFrame) -> pd.Series:
+    selected_index = random.choice(df.index)
+    selected_row = df.loc[selected_index]
+
+    # Cache the selected row index to ensure it won't be selected again
+    cached_indices = st.session_state.get('selected_indices', set())
+    cached_indices.add(selected_index)
+    st.session_state['selected_indices'] = cached_indices
+
+    return selected_row
+
+
+
+st.title("Success Enabler Search & Discovery Feedback Form")
+
+st.write("You will be given a search query followed by the results. "
+             "Provide a rating (5 being best) and feedback, click 'submit' and you will "
+             "be given a another to rate. When you are finished, simply exit this page.")
+
+df = import_dataframe()
+
+# Exclude already selected rows
+if 'selected_indices' in st.session_state:
+    remaining_indices = df.index.difference(st.session_state['selected_indices'])
+    df = df.loc[remaining_indices]
+
+if df.empty:
+    st.write("All rows have been reviewed!")
+else:
+    with st.form("feedback_form"):
+        selected_row = get_random_row(df)
+        st.write("Query:", selected_row["Input"])
+        st.write("Success Enablers Returned:", selected_row["Success Enablers"])
+        st.write("AI Summary: ", selected_row["Summary"])
+        employer = selected_row["Employer"]
+        if employer:
+            st.write("Employer: ", selected_row["Employer"])
+            st.write("Journeys", selected_row["Journeys"])
+        st.divider()
+        st.markdown("1. **Relevancy**: Are the Success Enablers relevant to the query? ")
+        relevancy_rating = st.slider("relevancy measure for Success Enablers", min_value=0, max_value=5)
+        relevancy_input = st.text_area()
+        st.markdown("2. **Accuracy**: Are there missing Success Enablers (even if you "
+                    "are not sure we offer them)?")
+        accuracy_rating = st.slider("accuracy measure for Success Enablers", min_value=0, max_value=5)
+        accuracy_input = st.text_area()
+
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            # Handle form submission
+            st.write("Thank you for your feedback!")
+
+
+    
