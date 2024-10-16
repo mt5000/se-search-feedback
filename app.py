642845928b73c1f8de9ddb848958dabec1e17471
mt5000 <matt@inclusively.com>
2024-10-16 13:40:42 -0400

    Update completion instructions in user prompt
    Clarify to users that they should click "I'm Done!" at the bottom of the page to finish. This improves user guidance and ensures they know the final step.

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ st.markdown("</div><div class='spacer'></div>", unsafe_allow_html=True)
 
 st.markdown(
     """
-    <p>You will be given a search query followed by the results. Provide a rating and feedback, click 'submit' and you will be given another to rate. When you are finished, simply exit this page.</p>
+    <p>You will be given a search query followed by the results. Provide a rating and feedback, click 'submit' and you will be given another to rate. When you are finished, click <b>I'm Done!</b> at the bottom of the page.</p>
     """,
     unsafe_allow_html=True
 )
