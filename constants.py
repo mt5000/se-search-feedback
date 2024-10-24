951ea3f6d83d0a86175b9b40108296316d53fcb3
mt5000 <matt@inclusively.com>
2024-10-24 10:03:10 -0400

    Refactor BigQuery sync mechanism
    Unified the query and feedback data merging into a single operation by updating the `push_to_bigquery` function. Removed redundant feedback list management and direct BigQuery sync with user feedback on submission.

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 question_2 = "**Are there missing Success Enablers (even if you are not sure we offer them)?**"
-question_1 = "**Are the Success Enablers relevant to the query?**"
+question_1 = ("**Are the Success Enablers relevant to the query? If No Success Enablers were returned, "
+              "was that the desired outcome?**")
 question_3 = "**Does the summary fulfill the user's intent and provide useful information?**"
